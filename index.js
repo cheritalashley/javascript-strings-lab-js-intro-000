817e0eb8608e31e290d0e53e73dfaf8dda2054a1
Cherita Lashley <lashleynook@gmail.com>
2018-08-29 16:37:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "Let\'s talk about space";
 
+<<<<<<< HEAD
 var conversation = `Let\'s talk about ${topic}.`;
+=======
+var conversation = `Let\'s talk about ${topic}`;
+>>>>>>> 20b7ed0e1bdcdf416dd862dbbb36fe0d597f9f9a
